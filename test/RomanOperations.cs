6c2aa9c434fa44578e04cfd99ae54f1a5e2dbf9a
Michel Grootjans <michel.grootjans@gmail.com>
2018-10-02 09:54:04 +0200

    added working implementation of roman numerals

---
+++ test/RomanOperations.cs
@@ -0,0 +1,38 @@
+using System.Linq;
+
+namespace Kata
+{
+    public class RomanOperations
+    {
+        public string Add(string left, string right)
+        {
+            return Normalize(Denormalise(left) + Denormalise(right));
+        }
+
+        private static string Denormalise(string roman)
+        {
+            return roman
+                .Replace("IV",  4.Times("I"))
+                .Replace( "V",  5.Times("I"))
+                .Replace("IX",  9.Times("I"))
+                .Replace( "X", 10.Times("I"));
+        }
+
+        private string Normalize(string denormalized)
+        {
+            return denormalized
+                .Replace(10.Times("I"), "X")
+                .Replace( 9.Times("I"), "IX")
+                .Replace( 5.Times("I"), "V")
+                .Replace( 4.Times("I"), "IV");
+        }
+    }
+
+    public static class IntExtensions
+    {
+        public static string Times(this int number, string repeatable)
+        {
+            return string.Join("", Enumerable.Repeat(repeatable, number));
+        }
+    }
+}
--- test/RomanOperationsTest.cs
+++ test/RomanOperationsTest.cs
@@ -4,15 +4,102 @@ namespace Kata
 {
     public class RomanOperationsTest
     {
+        private RomanOperations operations;
+
+        [SetUp]
+        public void SetUp()
+        {
+            operations = new RomanOperations();
+        }
+
         [Test]
         public void Add_I_I()
         {
-            Assert.That(Add("I", "I"), Is.EqualTo("II"));
+            Assert.That(operations.Add("I", "I"), Is.EqualTo("II"));
         }
 
-        private string Add(string s, string s1)
+        [Test]
+        public void Add_I_II()
         {
-            return "II";
+            Assert.That(operations.Add("I", "II"), Is.EqualTo("III"));
+        }
+
+        [Test]
+        public void Add_II_I()
+        {
+            Assert.That(operations.Add("II", "I"), Is.EqualTo("III"));
+        }
+
+        [Test]
+        public void Add_II_II()
+        {
+            Assert.That(operations.Add("II", "II"), Is.EqualTo("IV"));
+        }
+
+        [Test]
+        public void Add_II_III()
+        {
+            Assert.That(operations.Add("II", "III"), Is.EqualTo("V"));
+        }
+
+        [Test]
+        public void Add_I_IIII()
+        {
+            Assert.That(operations.Add("I", "IIII"), Is.EqualTo("V"));
+        }
+
+        [Test]
+        public void Add_III_III()
+        {
+            Assert.That(operations.Add("III", "III"), Is.EqualTo("VI"));
+        }
+
+        [Test]
+        public void Add_IV_I()
+        {
+            Assert.That(operations.Add("IV", "I"), Is.EqualTo("V"));
+        }
+
+        [Test]
+        public void Add_V_I()
+        {
+            Assert.That(operations.Add("V", "I"), Is.EqualTo("VI"));
+        }
+
+        [Test]
+        public void Add_VI_I()
+        {
+            Assert.That(operations.Add("VI", "I"), Is.EqualTo("VII"));
+        }
+
+        [Test]
+        public void Add_V_IV()
+        {
+            Assert.That(operations.Add("V", "IV"), Is.EqualTo("IX"));
+        }
+
+        [Test]
+        public void Add_VI_IV()
+        {
+            Assert.That(operations.Add("VI", "IV"), Is.EqualTo("X"));
+        }
+
+        [Test]
+        public void Add_VI_IX()
+        {
+            Assert.That(operations.Add("VI", "IX"), Is.EqualTo("XV"));
+        }
+
+        [Test]
+        public void Add_VI_X()
+        {
+            Assert.That(operations.Add("VI", "X"), Is.EqualTo("XVI"));
+        }
+
+        [Test]
+        public void Add_XIX_XX()
+        {
+            Assert.That(operations.Add("XIX", "XX"), Is.EqualTo("XXXIX"));
         }
     }
 }
